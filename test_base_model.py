c0abd8cc249f3943b80401fa778a47e78ad55b79
soft-insight <j0108rodriguez@gmail.com>
2022-07-05 12:06:26 -0500

    commit: 0

--- test_base_model.py
+++ test_base_model.py
@@ -3,18 +3,18 @@
 from models.base_model import BaseModel
 
 my_model = BaseModel()
-# my_model.name = "My First Model"
-# my_model.my_number = 89
+my_model.name = "My First Model"
+my_model.my_number = 89
 
 print(my_model)
-print()
+# print()
 my_model.save()
 print(my_model)
-print()
+# print()
 my_model_json = my_model.to_dict()
 print(type(my_model_json))
 print(my_model_json)
-print()
+# print()
 print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
