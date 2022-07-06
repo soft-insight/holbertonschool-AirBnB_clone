498044882c911fdbda2d2cda6694933e95f9fa8d
soft-insight <j0108rodriguez@gmail.com>
2022-07-05 19:50:27 -0500

    **kwargs dict as input

--- models/base_model.py
+++ models/base_model.py
@@ -18,16 +18,25 @@ class BaseModel:
                     updated
     """
 
-    def __init__(self):
-        """ the constructor of the BaseModel
+    def __init__(self, *args, **kwargs):
+        """ The constructor of the class BaseModel
             with attributes:
             id
             created_at
             updated_at
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        fmtDate = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for i in kwargs.keys():
+                if i == 'created_at' or i == 'updated_at':
+                    kwargs[i] = datetime.strptime(kwargs[i], fmtDate)
+                    setattr(self, i, kwargs[i])
+                if i != '__class__':
+                    setattr(self, i, kwargs[i])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Method that represents the class as string """
