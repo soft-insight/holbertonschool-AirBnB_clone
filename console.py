dffe839841e74a28392b17a3fe6ae452c656fc28
soft-insight <j0108rodriguez@gmail.com>
2022-07-05 22:50:01 -0500

    correct name

--- console.py
+++ console.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# AirBnB console
+""" AirBnB console """
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command line interface for AirBnB project """
 
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
     def do_quit(self, line):
         """Quit command to exit the program """
@@ -14,14 +14,10 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """Quit command to exit the program """
-        print()
         return True
 
     def emptyline(self):
-        """ called when empty line is entered.
-            If this method is not overridden,
-            it repeats the last nonempty command entered.
-        """
+        """ called when empty line is entered """
         pass
 
 
